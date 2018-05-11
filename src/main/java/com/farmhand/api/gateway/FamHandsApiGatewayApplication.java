5e35dd95f2bc45a752673302481647f2497a2e86
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-05-11 18:01:25 +0200

    Initial commit.

---
+++ src/main/java/com/farmhand/api/gateway/FamHandsApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.farmhand.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamHandsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamHandsApiGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/farmhand/api/gateway/FamHandsApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farmhand.api.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FamHandsApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
